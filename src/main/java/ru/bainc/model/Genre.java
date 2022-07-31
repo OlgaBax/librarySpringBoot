2e0df157588bd48543111e35307f60b21e36f4dc
Babushkina Olga <ba80@mail.ru>
2022-07-31 18:38:49 +0300

    Add model Genre

---
+++ src/main/java/ru/bainc/model/Genre.java
@@ -0,0 +1,23 @@
+package ru.bainc.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name="genres")
+public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="title")
+    private String genreTitle;
+
+}
