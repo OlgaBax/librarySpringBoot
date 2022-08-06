0298d64098a10bb4921aaa42abea17a2c84e7daf
Babushkina Olga <ba80@mail.ru>
2022-08-06 11:33:35 +0300

    Реализован класс GenreDto

---
+++ src/main/java/ru/bainc/dto/GenreDto.java
@@ -0,0 +1,22 @@
+package ru.bainc.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import ru.bainc.model.Genre;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class GenreDto {
+
+    private Long id;
+    private String title;
+
+
+    public GenreDto(Genre genre) {
+        this.id = genre.getId();
+        this.title = genre.getGenreTitle();
+    }
+}
